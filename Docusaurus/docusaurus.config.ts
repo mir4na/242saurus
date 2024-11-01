d69eaf5cf99b14ea9a0ae47aea6e190630963348
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-11-01 14:09:23 +0700

    pop quiz done

--- Docusaurus/docusaurus.config.ts
+++ Docusaurus/docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mir4na', // Usually your GitHub org/user name.
   projectName: '242saurus', // Usually your repo name.
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
